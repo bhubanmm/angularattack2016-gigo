5975cbf22b49300ec5c859ee9888d4aa2b406eeb
Bhuban Mohan Mishra <bhubanmmishra@gmail.com>
2016-05-15 11:55:57 +0530

    Updated Details View

--- app/app.entitydetails.js
+++ app/app.entitydetails.js
@@ -12,8 +12,9 @@
               "Pic": "http://www.history.co.uk/sites/default/files/bios/mahatmagandhi225.jpg"
           };
           this.Event = {
+            "Name": "Birth",
             "Date": "02/11/1869",
-            "Location": " Porbandar,Gujrat",
+            "Location": "Porbandar, Gujrat",
             "latitude": "21.641707",
             "longitude": "69.629265",
             "Description": "Mahatma Gandhi, was born on October 2, 1869, in Porbandar, Kathiawar, India, which was then part of the British Empire. His father, Karamchand Gandhi, served as a chief minister in Porbandar and other states in western India. His mother, Putlibai, was a deeply religious woman who fasted regularly. Gandhi grew up worshiping the Hindu god Vishnu and following Jainism, a morally rigorous ancient Indian religion that espoused non-violence, fasting, meditation and vegetarianism.",
--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 (function(app) {
   document.addEventListener('DOMContentLoaded', function() {
     ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+    ng.platformBrowserDynamic.bootstrap(app.EventDetailComponent);
+    ng.platformBrowserDynamic.bootstrap(app.TestComponent);
   });
 })(window.app || (window.app = {}));
